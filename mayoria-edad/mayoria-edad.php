9add86ab75889a608f7647f0a271c61671cb8023
Cristian Mauricio <cristianmauricioupc@gmail.com>
2025-03-10 21:27:27 -0500

    Privacidad y payoria de edad

---
+++ mayoria-edad/mayoria-edad.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anuncio de Mayoria de edad • FantaSexAnuncios.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="mayoria-edad.css">
+</head>
+<body class="body-mayoria-edad">
+    <div class="form-container">
+        <div class="text-center mb-4">
+            <img src="/assets/img/logos/logo.png" alt="Logo" class="rounded-circle" width="100">
+            <h4>Fanta<b style="color: #c60024;">SexAnuncios</b>.com</h4>
+            <h2>¡Atención! sitio con contenido para adultos</h2>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" style="font-weight: bold; font-size: 18px">Para acceder debes aceptar las siguientes condiciones:</label>
+        </div>
+        <ul class="mb-3" style="font-size: 14px">
+            <li class="form-label">Ser <b>mayor de edad</b> según la jurisdicción de tu país.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+        </u>
+        <div class="buttons-container">
+            <button class="auth-btn">Aceptar</button>
+            <button class="close-btn">Abandonar el sitio</button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ privacidad/privacidad.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Politicas de privacidad y cookies • FantaSexAnuncios.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="privacidad.css">
+</head>
+<body class="body-mayoria-edad">
+    <div class="form-container">
+        <div class="text-center mb-4">
+            <img src="/assets/img/logos/logo.png" alt="Logo" class="rounded-circle" width="100">
+            <h4>Fanta<b style="color: #c60024;">SexAnuncios</b>.com</h4>
+            <h2>TÉRMINOS DE USO, POLÍTICA DE PRIVACIDAD Y COOKIES</h2>
+        </div>
+        <div class="my-3">
+            <label class="mb-3 paragraph">
+                La titularidad de la página web con dominio www.nuevapasion.com es de Nueva Pasión SRL, con domicilio social en Avenue de la Chasse 133/135, 1040 Etterbeek, Bruxelles
+            </label>
+            <label class="mb-3 paragraph">
+                Toda persona que acceda a este sitio web asume el papel de usuario, adhiriéndose y aceptando todas las condiciones de uso y nuestra política de privacidad, comprometiéndose a respetar y cumplir estrictamente las disposiciones aquí dispuestas, así como cualquier otra disposición legal que fuera de aplicación. Le recomendamos que lea atentamente.
+            </label>
+            <label class="mb-3 paragraph">
+                <b class="paragraph">www.nuevapasion.com</b> se reserva el derecho a modificar cualquier tipo de información que pudiera aparecer en el sitio web, sin que exista obligación de advertir previamente o poner en conocimiento de los usuarios dichas obligaciones, entendiéndose como suficiente con la publicación en el sitio web de <b class="paragraph">www.nuevapasion.com</b>
+            </label>
+        </div>
+        <div class="mb-3">
+            <label class="form-label subtitle">1.- DEFINICIÓN DE SERVICIO</label>
+        </div>
+
+        <ul class="mb-3 paragraph">
+            <li class="form-label">Ser <b>mayor de edad</b> según la jurisdicción de tu país.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+            <li class="form-label">Permitir el uso de cookies propias y de terceros para tareas de análisis.</li>
+        </u>
+    </div>
+</body>
+</html>
