ef91f1e9fe9e3331852c5131183d7645bf35a407
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2025-03-27 23:03:48 -0500

    Fix Env

--- php/backend/config.php
+++ php/backend/config.php
@@ -1,7 +1,6 @@
 <?php
-$db_dsn = 'mysql:host=mysql; dbname=db6h0aovgdau0i';
-$db_username = 'ujjvh6bdg2gyn';
-$db_password = '1~l3}1Ke2^d$';
+
+require_once __DIR__ . '/../../config/config.php';
 
 try {
     $pdo = new PDO($db_dsn, $db_username, $db_password, [
